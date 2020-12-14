1cfe911058bb5115779f8cfa15a2f914e1c46973
vipin <vipin00147@gmail.com>
2020-12-14 12:56:03 +0530

    h

--- app/src/main/java/com/example/carwashapp/Admin_Home.java
+++ app/src/main/java/com/example/carwashapp/Admin_Home.java
@@ -1,16 +1,19 @@
 package com.example.carwashapp;
 
+import androidx.annotation.FloatRange;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.WindowManager;
 
-import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Admin_Home extends AppCompatActivity {
 
-    MeowBottomNavigation bottomNavigation;
+    BottomNavigationView navigation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
@@ -19,14 +22,33 @@ public class Admin_Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin__home);
 
-        bottomNavigation = (MeowBottomNavigation)findViewById(R.id.bottom_nav);
-
-        bottomNavigation.add(new MeowBottomNavigation.Model(1,R.drawable.order));
-        bottomNavigation.add(new MeowBottomNavigation.Model(2,R.drawable.add_user));
-        bottomNavigation.add(new MeowBottomNavigation.Model(3,R.drawable.car_wash));
-        bottomNavigation.add(new MeowBottomNavigation.Model(4,R.drawable.view_list));
-        bottomNavigation.add(new MeowBottomNavigation.Model(5,R.drawable.account_circle));
-
-        bottomNavigation.show(3,true);
+        navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(navListener);
     }
+     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+             new BottomNavigationView.OnNavigationItemSelectedListener() {
+                 @Override
+                 public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                     Fragment fragment = null;
+                     switch (item.getItemId()){
+                         case R.id.item1 :
+                             fragment = new AppointmentFragment();
+                             break;
+                         case R.id.item2 :
+                             fragment = new CreateUserFragment();
+                             break;
+                         case R.id.item3 :
+                             fragment = new AddServiceCenterFragment();
+                             break;
+                         case R.id.item4 :
+                             fragment = new ViewServiceCentersFragment();
+                             break;
+                         case  R.id.item5 :
+                             fragment = new profileFragment();
+                             break;
+                     }
+                     getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment).commit();
+                     return true;
+                 }
+             };
 }
