ada4bd7a5258fde3dab4988a433dfbff85fef669
vipin <vipin00147@gmail.com>
2020-12-16 13:21:15 +0530

    h

---
+++ app/src/main/java/com/example/carwashapp/Admin_Data.java
@@ -0,0 +1,44 @@
+package com.example.carwashapp;
+
+public class Admin_Data {
+    String email,phone,password,username;
+
+    public Admin_Data(String username, String email, String phone, String password) {
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
